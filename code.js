e8c1bf02fff16f4c5653652a09a405309a086ebd
bgayne <bgayne@gmail.com>
2018-06-29 16:26:24 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $(".price-item-link").on("click", function(e) {
+        e.preventDefault();
+        console.log("Hello?");
+        $(this).find("#dropdown-arrow").toggleClass("point-downwards");
+        $(this).children("#diagnostic-more-info").toggleClass("pricing-item-hidden");
+        console.log($("#diagnostic-more-info"));
+        console.log(this);
+    })
+
+    //Shamlessly taken from https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/
+    //Thanks
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
