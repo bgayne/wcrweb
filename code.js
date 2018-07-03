7f40f72cd3e1b1376890d42f54c7d0a950c36b47
bgayne <bgayne@gmail.com>
2018-07-03 11:45:19 -0400

    creating mobile nav

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+(}$(document).ready(function() {
     $(".price-item-link").on("click", function(e) {
         e.preventDefault();
         console.log("Hello?");
@@ -16,4 +16,14 @@ $(document).ready(function() {
             scrollTop: $($(this).attr('href')).offset().top
         }, 500, 'linear');
     });
+
+    $(".hamburger-menu").on("click", function(e) {
+        $("#ham-top").toggleClass("ham-top-rotate");
+        $("#ham-bottom").toggleClass("ham-bottom-rotate");
+        $("#ham-mid").toggleClass("hidden");
+        console.log("Test");
+        $(".mobile-nav").toggleClass("mobile-nav-expanded");
+        $("#mobile-nav-menu").toggleClass("hidden");
+    })
+
 });
