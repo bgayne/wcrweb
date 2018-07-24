e61ac9b7f37f7000c3b171e21ad81475b36a6cc3
Brandon Gayne <bgayne@gmail.com>
2018-07-23 20:34:00 -0400

    tidying up

--- contact.py
+++ contact.py
@@ -14,35 +14,23 @@ CORS(app)
 def main(filename):
     return send_file(filename)
 
-@app.route('/resources/<filename>')
-def resources(filename):
-    return send_from_directory('resources', filename)
+@app.route('/<dir>/<filename>')
+def resources(dir, filename):
+    return send_from_directory(dir, filename)
 
 @app.route('/', methods=['GET', 'POST'])
 def get_contact():
     if request.method == 'GET':
         return app.send_static_file('index.html')
     if request.method == 'POST':
-        print("Hello, world!")
+
         sg = sendgrid.SendGridAPIClient(apikey="")
         json = request.get_json(force=True)
         to_email = Email("brandon@winchestercomputers.com")
         from_email = Email(json['email'])
         subject = "New website inquiry"
-        print("From: {} \n \n Message: {} \n \n Email: {} ".format(json['name'], json['query'], json['email']))
         content = Content("text/plain", "From: {} \n \n Message: {} \n \n Email: {} ".format(json['name'], json['query'], json['email'])) 
 
-        print(from_email)
-        print(subject)
-        print(content)
-        print(to_email)
-
         mail = Mail(from_email, subject, to_email, content)
-        print(mail.get())
-        #response = sg.client.mail.send.post(request_body=mail.get())
-        
-        print(response.status_code)
-        print(response.body)
-        print(response.headers)
-        
+        response = sg.client.mail.send.post(request_body=mail.get())
         return 'Test'
